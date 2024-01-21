f798867910fe0b57c18293f745f853e0b426f127
LVHZV <94542882+LVHZV@users.noreply.github.com>
2024-01-21 16:53:42 +0800

    Add files via upload

--- EPA.cpp
+++ EPA.cpp
@@ -5,16 +5,16 @@ Vector2D GetUnFaceOriginVector(const Vector2D& a, const Vector2D& b)//获得线�
 {
 	Vector2D delta = a - b;
 	Vector2D nor =(Vector2D(delta.Y, -delta.X));
-	if (nor.dot(-a) < 0)
-		return nor;
-	return -nor;
+	if (nor.dot(a) < 0)
+		return -nor;
+	return nor;
 }
 
 struct Line
 {
 	Vector2D a;
 	Vector2D b;
-	float double_distance;
+	float distance;
 };
 
 Hit EPA(Triangle& semple, const std::vector<Vector2D>& object1, const std::vector<Vector2D>& object2)
@@ -30,13 +30,7 @@ Hit EPA(Triangle& semple, const std::vector<Vector2D>& object1, const std::vecto
 	for (int i = 0; i < 3;i++)
 	{
 		Vector2D dir = semple.point[i] - semple.point[(i+1)%3];
-		line_set[i].double_distance = std::abs(Math::Normalize(dir.GetNormal()).dot(semple.point[i]));
-		if (line_set[i].double_distance == 0.f)
-		{
-			hit.depth = 1;
-			hit.direct = Math::Normalize(dir.GetNormal());
-			return hit;
-		}
+		line_set[i].distance = std::abs(Math::Normalize(dir.GetNormal()).dot(semple.point[i]));
 	}
 
 	for (;;)
@@ -44,15 +38,19 @@ Hit EPA(Triangle& semple, const std::vector<Vector2D>& object1, const std::vecto
 		int index = 0;
 		for (int i = 1; i < line_set.size(); i++)
 		{
-			if (line_set[index].double_distance > line_set[i].double_distance)
+			if (line_set[index].distance > line_set[i].distance)
+			{
+				Vector2D dir = Math::Normalize(line_set[i].b - line_set[i].a);
+				if(dir.dot(line_set[i].a)* dir.dot(line_set[i].b)<=0)
 				index = i;
+			}
 			
 		}
 		Vector2D sch_dir = Math::Normalize(GetUnFaceOriginVector(line_set[index].a, line_set[index].b));
 		Vector2D minkowski_d = support(object1, object2, sch_dir);
 		if (minkowski_d == line_set[index].a || minkowski_d == line_set[index].b)
 		{
-			hit.depth = line_set[index].double_distance;
+			hit.depth = line_set[index].distance;
 			hit.direct = sch_dir;
 			return hit;
 		}
@@ -61,9 +59,9 @@ Hit EPA(Triangle& semple, const std::vector<Vector2D>& object1, const std::vecto
 		line_set[index].b = minkowski_d;
 
 		Vector2D dir = line_set[index].a - line_set[index].b;
-		line_set[index].double_distance = std::abs(Math::Normalize(dir.GetNormal()).dot(line_set[index].a));
+		line_set[index].distance = std::abs(Math::Normalize(dir.GetNormal()).dot(line_set[index].a));
 
 		dir = line_set[line_set.size() - 1].a - line_set[index].b;
-		line_set[line_set.size() - 1].double_distance = std::abs(Math::Normalize(dir.GetNormal()).dot(line_set[line_set.size() - 1].a));
+		line_set[line_set.size() - 1].distance = std::abs(Math::Normalize(dir.GetNormal()).dot(line_set[line_set.size() - 1].a));
 	}
 }
--- GJK.cpp
+++ GJK.cpp
@@ -4,35 +4,32 @@ Hit GJK(const std::vector<Vector2D>& convex1, const Vector2D origin1, const std:
 {
 	Hit hit;
 	hit.is_collision=false;
-	Vector2D direct =Math::Normalize(origin1 - origin2);
+	Vector2D direct ;
+	if (origin1 == origin2)
+		direct = Vector2D(1.f, 0);
+	else
+		direct = Math::Normalize(origin2 - origin1);
+
 	Triangle semple;
 	semple.point[0]=support(convex1, convex2, direct);
 	if (semple.point[0].dot(direct) < 0)
 		return hit;
-	semple.point[1] =support(convex1, convex2, -direct);
-	if (semple.point[1].dot(-direct) < 0)
+	semple.point[1] =support(convex1, convex2, -semple.point[0]);
+	if (semple.point[1].dot(-semple.point[0]) < 0)
 		return hit;
-	direct = GetFaceOriginVector(semple.point[1], semple.point[0]);
-	Vector2D minkowski_d = support(convex1, convex2, direct);
-	if (minkowski_d.dot(direct) < 0)
-		return hit;
-	semple.point[2] = minkowski_d;
-
 	for (;;)
 	{
-		if (OriginInTriangle(semple))
-			return EPA(semple,convex1,convex2);
-
-		direct = GetFaceOriginVector(semple.point[1], semple.point[2]);
+		direct = GetFaceOriginVector(semple.point[1], semple.point[0]);
 		Vector2D minkowski_d = support(convex1, convex2, direct);
+
+		semple.point[2] = minkowski_d;
+
 		if (minkowski_d.dot(direct) < 0)
 			return hit;
 		if (minkowski_d == semple.point[1]|| minkowski_d == semple.point[2])
 			return hit;
-		semple.point[0] = semple.point[1];
-		semple.point[1] = semple.point[2];
-		semple.point[2] = minkowski_d;
-			
+		if (OriginInTriangle(semple))
+			return EPA(semple,convex1,convex2);	
 	}
 }
 
@@ -40,10 +37,10 @@ Vector2D support(const std::vector<Vector2D>& convex1, const std::vector<Vector2
 {
 	Vector2D A, B;
 	A = convex1[0];
-	float distance = direct.dot(convex1[0]);
+	double distance = direct.dot(convex1[0]);
 	for (std::size_t i = 1; i < convex1.size(); i++)
 	{
-		float t = direct.dot(convex1[i]);
+		double t = direct.dot(convex1[i]);
 		if (t > distance)
 		{
 			distance = t;
@@ -56,7 +53,7 @@ Vector2D support(const std::vector<Vector2D>& convex1, const std::vector<Vector2
 	distance = n_direct.dot(convex2[0]);
 	for (std::size_t i = 1; i < convex2.size(); i++)
 	{
-		float t = n_direct.dot(convex2[i]);
+		double t = n_direct.dot(convex2[i]);
 		if (t > distance)
 		{
 			distance = t;
@@ -67,17 +64,24 @@ Vector2D support(const std::vector<Vector2D>& convex1, const std::vector<Vector2
 }
 
 //检测三角形是否包含原点
-bool OriginInTriangle(const Triangle& semple)
+bool OriginInTriangle(Triangle& semple)
 {
 	Vector2D AB = semple.point[1] - semple.point[2];
 	Vector2D AC = semple.point[0] - semple.point[2];
 	Vector2D AO = -semple.point[2];
 	Vector2D nor =Math::Normalize(Math::TripleCorss(AB,AC,AC));
 	if (nor.dot(AO) >= 0)
-		return false;
+		{
+			semple.point[1] = semple.point[2];
+			return false;
+		}
 	nor = Math::Normalize(Math::TripleCorss(AC, AB, AB));
 	if (nor.dot(AO) >= 0)
-		return false;
+		{
+			semple.point[0] = semple.point[1];
+			semple.point[1] = semple.point[2];
+			return false;
+		}
 	return true;
 }
 
